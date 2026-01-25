8027d79aa36549187f0b571be00d4c50c313b031
MatiC25 <mcao@frba.utn.edu.ar>
2026-01-24 23:49:13 -0300

    Hotfix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/i-cloud-ventas/',
-  resolve: {              // <--- 2. AGREGA ESTO
+  base: '/', // <--- CAMBIO CRÍTICO: Debe ser la raíz para Vercel
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
